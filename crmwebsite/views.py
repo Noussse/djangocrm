c8743d42cb8e79fc9910f8d45158d60706468141
Noussse <149839925+Noussse@users.noreply.github.com>
2025-03-09 21:17:24 +0100

    finished the register form

--- crmwebsite/views.py
+++ crmwebsite/views.py
@@ -36,3 +36,4 @@ def register_user(request):
     else:
         form=SignUpForm()
         return render(request,'register.html',{'form':form})
+    return render(request,'register.html',{'form':form})
